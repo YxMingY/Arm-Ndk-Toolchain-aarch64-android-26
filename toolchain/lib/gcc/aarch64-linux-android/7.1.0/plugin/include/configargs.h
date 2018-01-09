c6e0af647ceb05de9a2f13dc0a2bff8914849090
Saint-Theana <uiniui@126.com>
2018-01-09 18:28:47 +0800

    备注信息

---
+++ toolchain/lib/gcc/aarch64-linux-android/7.1.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc-7.1.0/configure --host=aarch64-linux-android --prefix=/media/a4455jkjh/android/gcc/temp-install --enable-gold --enable-plugins --datarootdir=/data/data/com.aide.ui/files/ndksupport-20160121/android-ndk-aide/prebuilt/linux-arm64/share --with-system-zlib --target=aarch64-linux-android --with-sysroot=/media/a4455jkjh/android/gcc/temp-install/sysroot --disable-shared --disable-bootstrap --enable-bionic-libs --enable-languages=c,c++ --disable-tls --with-gxx-include-dir=/media/a4455jkjh/android/gcc/temp-install/include/c++/7.1.0 --disable-libsanitizer --disable-libquadmath --disable-libcilkrts --enable-thread=posix";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
